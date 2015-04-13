4048aaf5ac502f7b04fa270aa0e3e04ee0d01a50
Andrea Velazquez <avelazq1@hawk.iit.edu>
2015-04-13 04:11:05 -0500

    remove standalone model that was previously created, conflicted with model created in scaffold version

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150413044812) do
+ActiveRecord::Schema.define(version: 20150413085050) do
 
   create_table "droplets", force: :cascade do |t|
     t.integer  "drink"
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "weeks", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
