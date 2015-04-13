e84a276fdf411f9a8cb099d26711ffc93b95caa5
Andrea Velazquez <avelazq1@hawk.iit.edu>
2015-04-13 02:20:27 -0500

    rake db:seed create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#Droplets
+Droplet.create!(drink:'1', description:'glass of water')
+Droplet.create!(drink:'2', description:'two glasses of water during work')
+Droplet.create!(drink:'3', description:'water consumed during yoga')
+Droplet.create!(drink:'5', description:'meant to say 5 instead of 50') 
+Droplet.create!(drink:'25', description:'had a lot of water today!')
+
+
+
+
